1dd4c52aa9052b010b35562145df17e685838655
Kishore Kumar <kishorekumar.jya@gmail.com>
2023-06-08 01:31:42 +0530

    Generate a model

---
+++ Generate a model.php
@@ -0,0 +1,31 @@
+<?php
+You may generate various other types of classes when generating a model, such as factories, seeders, policies, controllers, and form requests. In addition, these options may be combined to create multiple classes at once:
+
+# Generate a model and a FlightFactory class...
+php artisan make:model Flight --factory
+php artisan make:model Flight -f
+ 
+# Generate a model and a FlightSeeder class...
+php artisan make:model Flight --seed
+php artisan make:model Flight -s
+ 
+# Generate a model and a FlightController class...
+php artisan make:model Flight --controller
+php artisan make:model Flight -c
+ 
+# Generate a model, FlightController resource class, and form request classes...
+php artisan make:model Flight --controller --resource --requests
+php artisan make:model Flight -crR
+ 
+# Generate a model and a FlightPolicy class...
+php artisan make:model Flight --policy
+ 
+# Generate a model and a migration, factory, seeder, and controller...
+php artisan make:model Flight -mfsc
+ 
+# Shortcut to generate a model, migration, factory, seeder, policy, controller, and form requests...
+php artisan make:model Flight --all
+ 
+# Generate a pivot model...
+php artisan make:model Member --pivot
+php artisan make:model Member -p
